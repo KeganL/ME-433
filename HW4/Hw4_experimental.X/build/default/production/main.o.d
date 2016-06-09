45e70328bf2ff08d5857954e549d76cd576c5ca7
KeganL <keganleizerman2016@u.northwestern.edu>
2016-06-08 22:28:13 -0500

    1-4, 8-10, 12
    Still needs IMU, LCD, Mouse, Box; CDC android, tech cup combo app, and
    racer code

---
+++ HW4/Hw4_experimental.X/build/default/production/main.o.d
@@ -0,0 +1,5 @@
+build/default/production/main.o:  \
+main.c  \
+PIC32_INIT.h  \
+i2c_master_noint.h  \
+ pin_exp.h 
--- usb-serial-for-android-master/usbSerialExamples/src/main/java/src/com/hoho/android/usbserial/examples/CdcAcmSerialDriver.java
+++
@@ -1,7 +0,0 @@
-package src.com.hoho.android.usbserial.examples;
-
-/**
- * Created by Admin on 6/6/16.
- */
-public class CdcAcmSerialDriver {
-}
